35a37165df02cd6626be1a6360b58da8af1c0122
alvarg93 <alvarglj@gmail.com>
2015-11-21 16:39:13 +0100

    dispatchers ondataobtained

--- app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/ProjectsRequestDispatcher.java
+++ app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/ProjectsRequestDispatcher.java
@@ -1,5 +1,14 @@
 package com.mooduplabs.taigaopensource.requestdispatchers;
 
+import com.mooduplabs.taigaopensource.events.IssuesReadyEvent;
+import com.mooduplabs.taigaopensource.events.ListItemsReadyEvent;
+import com.mooduplabs.taigaopensource.utils.BusHelper;
+import com.mooduplabs.taigaopensource.viewmodels.ListItemsViewModel;
+
+import org.json.JSONObject;
+
+import de.halfbit.tinybus.Subscribe;
+
 /**
  * Created by �ukasz on 2015-11-21.
  */
@@ -9,5 +18,19 @@ public class ProjectsRequestDispatcher extends BaseRequestDispatcher {
         super();
         this.endpoint = "projectsendpoint";
     }
+
+    @Subscribe
+    private void onDataObtained(IssuesReadyEvent event) {
+        ListItemsReadyEvent itemsReadyEvent = new ListItemsReadyEvent();
+        itemsReadyEvent.listItemsViewModels = prepareViewModels(event.jsonData);
+        BusHelper.getInstance().post(itemsReadyEvent);
+        BusHelper.getInstance().unregister(this);
+    }
+
+    private ListItemsViewModel prepareViewModels(JSONObject jsonData) {
+        //jsonData.getJSONArray("projects")
+        return new ListItemsViewModel();
+    }
+
 
 }
--- app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/TasksRequestDispatcher.java
+++ app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/TasksRequestDispatcher.java
@@ -1,5 +1,14 @@
 package com.mooduplabs.taigaopensource.requestdispatchers;
 
+import com.mooduplabs.taigaopensource.events.IssuesReadyEvent;
+import com.mooduplabs.taigaopensource.events.ListItemsReadyEvent;
+import com.mooduplabs.taigaopensource.utils.BusHelper;
+import com.mooduplabs.taigaopensource.viewmodels.ListItemsViewModel;
+
+import org.json.JSONObject;
+
+import de.halfbit.tinybus.Subscribe;
+
 /**
  * Created by �ukasz on 2015-11-21.
  */
@@ -9,5 +18,19 @@ public class TasksRequestDispatcher extends BaseRequestDispatcher {
         super();
         this.endpoint = "tasksendpoint";
     }
+
+    @Subscribe
+    private void onDataObtained(IssuesReadyEvent event) {
+        ListItemsReadyEvent itemsReadyEvent = new ListItemsReadyEvent();
+        itemsReadyEvent.listItemsViewModels = prepareViewModels(event.jsonData);
+        BusHelper.getInstance().post(itemsReadyEvent);
+        BusHelper.getInstance().unregister(this);
+    }
+
+    private ListItemsViewModel prepareViewModels(JSONObject jsonData) {
+        //jsonData.getJSONArray("tasks")
+        return new ListItemsViewModel();
+    }
+
 
 }
