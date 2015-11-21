aedb98c1874ce8896b6d13bdd932e35b299d8cd4
MichaalOlejniczak <michal.olejniczak92@gmail.com>
2015-11-21 14:25:10 +0100

    created three fragments

---
+++ app/src/main/java/com/mooduplabs/taigaopensource/fragments/IssueDetails.java
@@ -0,0 +1,27 @@
+package com.mooduplabs.taigaopensource.fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mooduplabs.taigaopensource.R;
+
+public class IssueDetails extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_issue_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/com/mooduplabs/taigaopensource/fragments/TaskDetails.java
@@ -0,0 +1,32 @@
+package com.mooduplabs.taigaopensource.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mooduplabs.taigaopensource.R;
+
+public class TaskDetails extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
