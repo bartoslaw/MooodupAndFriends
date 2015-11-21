61fb6023847e7657e0d93d410dc8138998992c44
MichaalOlejniczak <michal.olejniczak92@gmail.com>
2015-11-21 15:46:35 +0100

    quickFix

--- app/src/main/java/com/mooduplabs/taigaopensource/fragments/TaskDetails.java
+++ app/src/main/java/com/mooduplabs/taigaopensource/fragments/TaskDetails.java
@@ -3,5 +3,12 @@ package com.mooduplabs.taigaopensource.fragments;
  * Created by Michal on 2015-11-21.
  */
 public class TaskDetails extends BaseFragment {
+    @Override
+    int getLayoutRes() {
+        return 0;
+    }
 
+    @Override
+    void onFragmentCreated() {
+    }
 }
