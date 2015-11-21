13d11082ab064123e2efd2937d08c83f1c5c275e
alvarg93 <alvarglj@gmail.com>
2015-11-21 14:38:50 +0100

    remaining list requestdispatchers

---
+++ app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/IssuesRequestDispatcher.java
@@ -0,0 +1,12 @@
+package com.mooduplabs.taigaopensource.requestdispatchers;
+
+/**
+ * Created by £ukasz on 2015-11-21.
+ */
+public class IssuesRequestDispatcher extends BaseRequestDispatcher {
+
+    public IssuesRequestDispatcher() {
+        this.endpoint = "issuesendpoint";
+    }
+
+}
