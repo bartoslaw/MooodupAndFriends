8edaac6770ce83888c6ff5921b08ac88e7525193
Bartol <bs.smektala@gmail.com>
2015-11-21 14:55:16 +0100

    base request dispatchter injected with http client

--- app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/BaseRequestDispatcher.java
+++ app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/BaseRequestDispatcher.java
@@ -1,7 +1,20 @@
 package com.mooduplabs.taigaopensource.requestdispatchers;
 
+import com.mooduplabs.taigaopensource.backend.HttpClientService;
+import com.mooduplabs.taigaopensource.components.DaggerHttpClientComponent;
+import com.mooduplabs.taigaopensource.modules.HttpClientModule;
+
+import javax.inject.Inject;
+
 public class BaseRequestDispatcher {
 
-    String endpoint;
+    @Inject
+    protected HttpClientService httpClientService;
+
+    protected String endpoint;
+
+    protected BaseRequestDispatcher() {
+        DaggerHttpClientComponent.builder().httpClientModule(new HttpClientModule(false)).build();
+    }
 
 }
--- app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/TasksRequestDispatcher.java
+++ app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/TasksRequestDispatcher.java
@@ -1,11 +1,12 @@
 package com.mooduplabs.taigaopensource.requestdispatchers;
 
 /**
- * Created by £ukasz on 2015-11-21.
+ * Created by ï¿½ukasz on 2015-11-21.
  */
 public class TasksRequestDispatcher extends BaseRequestDispatcher {
 
     public TasksRequestDispatcher() {
+        super();
         this.endpoint = "tasksendpoint";
     }
 
