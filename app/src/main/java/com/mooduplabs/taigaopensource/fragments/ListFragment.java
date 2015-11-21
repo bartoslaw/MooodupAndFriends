b53d076e8841eea7b87233d9c06f36d4fa6e04c9
Sebastian Matysiak <seba.maty@gmail.com>
2015-11-21 14:59:48 +0100

    Łukasz authorship removed due to encoding issues xD

--- app/src/main/java/com/mooduplabs/taigaopensource/fragments/ListFragment.java
+++ app/src/main/java/com/mooduplabs/taigaopensource/fragments/ListFragment.java
@@ -1,7 +1,4 @@
 package com.mooduplabs.taigaopensource.fragments;
 
-/**
- * Created by £ukasz on 2015-11-21.
- */
 public class ListFragment extends BaseFragment {
 }
--- app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/ProjectsRequestDispatcher.java
+++ app/src/main/java/com/mooduplabs/taigaopensource/requestdispatchers/ProjectsRequestDispatcher.java
@@ -1,7 +1,4 @@
 package com.mooduplabs.taigaopensource.requestdispatchers;
 
-/**
- * Created by £ukasz on 2015-11-21.
- */
 public class ProjectsRequestDispatcher extends BaseRequestDispatcher {
 }
